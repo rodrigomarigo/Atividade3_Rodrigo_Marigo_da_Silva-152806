32b2911e25b284dc30349691b7b2b1037d434f9c
rodrigomarigo <rodrigomarigo@gmail.com>
2020-05-21 20:47:16 -0300

    first commit

---
+++ src/test/java/com/example/atividade03_rodrigo_marigo_da_silva_152806/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.atividade03_rodrigo_marigo_da_silva_152806;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
