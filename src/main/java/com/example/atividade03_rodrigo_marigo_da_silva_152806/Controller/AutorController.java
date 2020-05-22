37b50370ced684a0237db7b26afc33272e30c21e
rodrigomarigo <rodrigomarigo@gmail.com>
2020-05-22 15:07:05 -0300

    Revisado algumas partes do código e algumas inconsistências foram alteradas

--- src/main/java/com/example/atividade03_rodrigo_marigo_da_silva_152806/Controller/AutorController.java
+++ src/main/java/com/example/atividade03_rodrigo_marigo_da_silva_152806/Controller/AutorController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import com.example.atividade03_rodrigo_marigo_da_silva_152806.Entity.Autor;
 import com.example.atividade03_rodrigo_marigo_da_silva_152806.Entity.Livro;
 import com.example.atividade03_rodrigo_marigo_da_silva_152806.Service.AutorService;
-import com.example.atividade03_rodrigo_marigo_da_silva_152806.Service.EditoraService;
 import com.example.atividade03_rodrigo_marigo_da_silva_152806.Service.LivroService;
 
 import org.springframework.beans.factory.annotation.Autowired;
