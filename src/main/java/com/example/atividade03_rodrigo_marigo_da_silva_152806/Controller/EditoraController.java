1212a85e9cfa61e8f81299aba918b07ece2dd21c
rodrigomarigo <rodrigomarigo@gmail.com>
2020-05-22 13:33:22 -0300

    Métodos getEditoras e salvar criados em EditoraController

--- src/main/java/com/example/atividade03_rodrigo_marigo_da_silva_152806/Controller/EditoraController.java
+++ src/main/java/com/example/atividade03_rodrigo_marigo_da_silva_152806/Controller/EditoraController.java
@@ -1,20 +1,36 @@
 package com.example.atividade03_rodrigo_marigo_da_silva_152806.Controller;
 
+import com.example.atividade03_rodrigo_marigo_da_silva_152806.Entity.Editora;
 import com.example.atividade03_rodrigo_marigo_da_silva_152806.Service.AutorService;
 import com.example.atividade03_rodrigo_marigo_da_silva_152806.Service.EditoraService;
 import com.example.atividade03_rodrigo_marigo_da_silva_152806.Service.LivroService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 public class EditoraController {
 
     @Autowired
     private EditoraService editoraService;
 
-    @Autowired
-    private LivroService livroService;
+    @GetMapping("/editoras")
+    public ModelAndView getEditoras() {
+        ModelAndView mv = new ModelAndView("editorasTemplate");
+        
+        mv.addObject("editora", new Editora());
+        mv.addObject("editoras", editoraService.getEditoras());
 
-    @Autowired
-    private AutorService autorService;
+        return mv;
+    }
+    
+    @PostMapping("/salvarEditora")
+    public String salvar(@ModelAttribute Editora editora){
+        editoraService.salvar(editora);
+
+        return "redirect:/editoras";
+    }
     
 }
