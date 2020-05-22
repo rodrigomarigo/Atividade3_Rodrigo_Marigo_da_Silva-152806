846da3eb273f7fc5f92f1db1a153b0d694237ea8
rodrigomarigo <rodrigomarigo@gmail.com>
2020-05-22 13:56:17 -0300

    Adicionado o método detalhesEditora em EditoraController

--- src/main/java/com/example/atividade03_rodrigo_marigo_da_silva_152806/Controller/EditoraController.java
+++ src/main/java/com/example/atividade03_rodrigo_marigo_da_silva_152806/Controller/EditoraController.java
@@ -1,13 +1,12 @@
 package com.example.atividade03_rodrigo_marigo_da_silva_152806.Controller;
 
 import com.example.atividade03_rodrigo_marigo_da_silva_152806.Entity.Editora;
-import com.example.atividade03_rodrigo_marigo_da_silva_152806.Service.AutorService;
 import com.example.atividade03_rodrigo_marigo_da_silva_152806.Service.EditoraService;
-import com.example.atividade03_rodrigo_marigo_da_silva_152806.Service.LivroService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -25,6 +24,17 @@ public class EditoraController {
 
         return mv;
     }
+
+    @GetMapping("/detalhesEditora/{codigo}")
+    public ModelAndView getEditoraDetalhes(@PathVariable(name = "codigo") Integer codigo) {
+
+        Editora editora = editoraService.getEditoraById(codigo);
+        ModelAndView mv = new ModelAndView("detalhesEditora");
+        mv.addObject("editora", editora);
+
+        return mv;
+    }
+
     
     @PostMapping("/salvarEditora")
     public String salvar(@ModelAttribute Editora editora){
