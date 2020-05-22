5128902e2197bc5a55824dd3177f894eda4db052
rodrigomarigo <rodrigomarigo@gmail.com>
2020-05-22 13:16:13 -0300

    Criado o template detalhesLivro e ajustado o controle de Autor

--- src/main/java/com/example/atividade03_rodrigo_marigo_da_silva_152806/Controller/AutorController.java
+++ src/main/java/com/example/atividade03_rodrigo_marigo_da_silva_152806/Controller/AutorController.java
@@ -1,11 +1,16 @@
 package com.example.atividade03_rodrigo_marigo_da_silva_152806.Controller;
 
+import com.example.atividade03_rodrigo_marigo_da_silva_152806.Entity.Autor;
 import com.example.atividade03_rodrigo_marigo_da_silva_152806.Service.AutorService;
 import com.example.atividade03_rodrigo_marigo_da_silva_152806.Service.EditoraService;
 import com.example.atividade03_rodrigo_marigo_da_silva_152806.Service.LivroService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AutorController {
@@ -13,11 +18,21 @@ public class AutorController {
     @Autowired
     private AutorService autorService;
 
-    @Autowired
-    private LivroService livroService;
+    @GetMapping("/autores")
+    public ModelAndView getAutores() {
+        ModelAndView mv = new ModelAndView("autoresTemplate");
+        
+        mv.addObject("autor", new Autor());
+        mv.addObject("autores", autorService.getAutores());
 
-    @Autowired
-    private EditoraService editoraService;  
+        return mv;
+    }
+    
+    @PostMapping("/salvarAutor")
+    public String salvar(@ModelAttribute Autor autor){
+        autorService.salvar(autor);
 
+        return "redirect:/autores";
+    }
     
 }
